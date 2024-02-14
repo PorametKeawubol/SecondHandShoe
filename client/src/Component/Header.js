9b0723b9bd69597fbb799397285ef3418a0ccc12
Lakztv <lakztvv@gmail.com>
2024-02-14 22:42:36 +0700

    change header

--- client/src/Component/Header.js
+++ client/src/Component/Header.js
@@ -4,40 +4,22 @@ import { Dialog} from '@headlessui/react'
 
 
 export default function Example() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-
   return (
-    <header className="bg-white">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <img className="h-8 w-auto" src={logo} alt="Logo" />
-          </a>
-        </div>
-        
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
-          </a>
-        </div>
-      </nav>
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
-              </div>
-            </div>
+    <header className="bg-black">
+      <div className="px-4 lg:px-8">
+        <nav className="flex items-center justify-between mx-auto py-4" aria-label="Global">
+          <div className="flex lg:flex-1">
+            <a href="#">
+              <img className="h-20 w-auto" src={logo} alt="Logo" />
+            </a>
           </div>
-        </Dialog.Panel>
-      </Dialog>
+          <div>
+            <a href="#" className="font-semibold leading-6 text-white">
+              Log in <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </nav>
+      </div>
     </header>
   )
 }
