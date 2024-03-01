158a3ad4a8d751f32a5b1bee2e1710382a2da896
Chotmanat Chanakul <6610110074@email.psu.ac.th>
2024-03-02 03:13:25 +0700

    now show data

--- client/src/AllPage/Adminpage.js
+++ client/src/AllPage/Adminpage.js
@@ -3,21 +3,36 @@ import Footer from '../Component/Footer';
 import Header from "../Component/Header";
 import axios from 'axios';
 import { useState,useEffect } from 'react';
+import PaymentList from '../Component/PaymentList';
 
 export default function Admin(){
     const [ListShoes,setListShoes] = useState("")
-    
+    console.log("🚀 ~ Admin ~ ListShoes:", ListShoes)
+    axios.defaults.headers.common["Authorization"] = `Bearer ${sessionStorage.getItem("authToken")}`
     useEffect(() => {
         fetchList();
       }, []);
     const fetchList = async() =>{
         try{
-            const response = await axios.get('api/payments?populate=*',{
-                headers: {
-                    Authorization: `Bearer ${sessionStorage.getItem("authToken")}`,
-                },
-            });
+            const response = await axios.get('api/payments?populate=*');
             console.log("🚀 ~ fetchList ~ response:", response)
+            if (Array.isArray(response.data.data)){
+                const ListData = response.data.data.map((List)=>{
+                    const { id, attributes } = List;
+                    const{shoe,Buyer,Bill,Confirm}=attributes;
+                    const bill = "http://localhost:1337" + Bill.data.attributes.url
+                    const shoe_id = shoe.data.id
+                    const Buyer_id = Buyer.data.id
+                    return({
+                        id,bill,Confirm,shoe_id,Buyer_id
+                    })
+                })
+                const filteredListData = ListData.filter((List)=>{
+                    return List.Confirm === false
+                })
+                setListShoes(filteredListData)
+            }   
+            
         }catch(error) {
             console.error("Error fetching shoes:", error);
         }
@@ -32,17 +47,10 @@ export default function Admin(){
                     <div className='text-slate-600 h-20 p-5 text-xl font-bold'>
                         <p className=''>confirmation</p>
                     </div>
-                    <div className='text-slate-600  p-5 text-lg'>
-                        <p className=''>confirmation</p>
-                    </div>
-                    <div className='text-slate-600  p-5 text-lg'>
-                        <p className=''>confirmation</p>
-                    </div>
-                    <div className='text-slate-600  p-5 text-lg'>
-                        <p className=''>confirmation</p>
-                    </div>
-                    <div className='text-slate-600  p-5 text-lg'>
-                        <p className=''>confirmation</p>
+                    <div>
+                        {ListShoes&&ListShoes.map((List)=>{
+                            return <PaymentList item={List}/>
+                        })}
                     </div>
 
                 </div>
--- client/src/Component/PaymentList.js
+++ client/src/Component/PaymentList.js
@@ -1,77 +1,46 @@
 import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { IoMdAdd, IoMdClose, IoMdRemove } from "react-icons/io";
-import { CartContext } from "../contexts/CartContext";
+import { ShoeContext } from "../contexts/ShoeContext";
 
-export default function PaymentList ({ item }) {
-    const { removeFromCart, increaseAmount, decreaseAmount } =
-      useContext(CartContext);
-    const navigate = useNavigate();
-  
-    const { id, products_name, image, price, amount } = item;
-  
-    if (item.amount > 1) {
-      item.amount = 1;
-    }
-  
+export default function PaymentList({ item }) {
+    const { shoes } = useContext(ShoeContext);
+    console.log("🚀 ~ PaymentList ~ shoes:", shoes);
+    const id = item.id;
+    const Buyer_id = item.Buyer_id;
+    const Confirm = item.Confirm;
+    const shoe_id = item.shoe_id;
+    const shoe = shoes.filter((item) => {
+        return item.id === shoe_id;
+    });
+    console.log("🚀 ~ shoe ~ shoe:", shoe);
     return (
-      <div className="flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500">
-        <div className="w-full min-h-[150px] flex items-center gap-x-4">
-          <Link to={`/shoe/${id}`}>
-            <img className="max-w-[80px]" src={image[0]} alt="" />
-          </Link>
-          <div className="w-full flex flex-col">
-            <div className="flex justify-between mb-2">
-              <Link
-                to={`/shoe/${id}`}
-                className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
-              >
-                {products_name}
-              </Link>
-              <div
-                onClick={() => removeFromCart(id)}
-                className="text-xl cursor-pointer"
-              >
-                <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
-              </div>
+        <div className="flex flex-row gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500">
+            <div className="w-full min-h-[150px] flex items-center gap-x-4 pl-10">
+                <img className="max-w-[80px]" src={shoe[0].image[0]} alt="" />
+
+                <div className="w-full flex flex-row p-6">
+                    <div className="flex justify-between mb-2">
+                        <Link
+                            to={`/shoe/${id}`}
+                            className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
+                        >
+                            {shoe[0].products_name}
+                        </Link>
+                    </div>
+                    <div className="flex gap-x-2 h-[36px] text-sm"></div>
+                    <div className="flex flex-1 justify-around items-center">
+                        {shoe[0].price} THB
+                    </div>
+                    <div className="flex justify-center items-center bg-green-200 text-emerald-500 font-medium cursor-pointer w-20 h-8 rounded-md ml-2">
+                        Accepted
+                    </div>
+                    <div className="flex justify-center items-center bg-red-200 text-rose-500 font-medium cursor-pointer w-20 h-8 rounded-md ml-2">
+                        Denied
+                    </div>
+                    <Link></Link>
+                </div>
             </div>
-            <div className="flex gap-x-2 h-[36px] text-sm">
-              <div className="flex flex-1 max-w-[100px] items-center h-full border text-primary font-medium">
-                <div
-                  onClick={() => decreaseAmount(id)}
-                  className="h-full flex-1 flex justify-center items-center cursor-pointer"
-                >
-                  <IoMdRemove />
-                </div>
-                <div className="h-full flex justify-center items-center px-2">
-                  {amount}
-                </div>
-                <div
-                  onClick={() => increaseAmount(id)}
-                  className="h-full flex flex-1 justify-center items-center cursor-pointer"
-                >
-                  <IoMdAdd />
-                </div>
-              </div>
-              <div className="flex flex-1 justify-around items-center">
-                {price} THB
-              </div>
-              <div className="flex flex-1 justify-end items-center text-primary font-medium">
-                {" "}
-                THB
-                {parseFloat(price * amount)
-                  .toFixed(2)
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-              </div>
-              {/* <Link to={`/Payment/${id}`}> */}
-                <div className="flex justify-center items-center bg-black text-white font-medium cursor-pointer w-20 h-8 rounded-md ml-2">
-                  Checkout
-                </div>
-              {/* </Link> */}
-            </div>
-          </div>
         </div>
-      </div>
     );
-  };
+}
