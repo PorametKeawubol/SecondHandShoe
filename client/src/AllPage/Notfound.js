2f3e09bf827fa56cad6a259b607af3de3eb662f6
Chotmanat Chanakul <6610110074@email.psu.ac.th>
2024-03-08 16:28:17 +0700

    not found

--- client/src/AllPage/Notfound.js
+++ client/src/AllPage/Notfound.js
@@ -1,16 +1,20 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import Header from "../Component/Header";
 
 function NotFound() {
-  return (
-    <div className="backgroundAll">
-      <div>
-        <p>404 Not Found</p>
-        <p>404 Not Found</p>
-        <p>404 Not Found</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-col   backgroundAll ">
+            <Header />
+            <div className="flex flex-col  justify-center	 h-screen items-center">
+                <p className="text-xl font-bold text-blue-900">404 Not Found</p>
+                <Link to={"/"}>
+                    <p className="text-sm p-4 font-bold text-blue-900 hover:text-black hover:underline-offset-2 ">
+                        {"return to HomePage ->"}
+                    </p>
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export default NotFound;
